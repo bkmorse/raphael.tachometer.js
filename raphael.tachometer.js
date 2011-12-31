8621b69135dfadb7f1636d4894c66545a8e0550d
tadashi-tanaka <t.tanaka@code-front.com>
2011-12-31 12:33:54 +0900

    enable to set needle position interactively.

--- raphael.tachometer.js
+++ raphael.tachometer.js
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------
-// raphael.tachometer.js 1.0.0
+// raphael.tachometer.js 1.1.0
 // 
 // Copyright (c) 2011 Code Front (http://code-front.com)
 // Licensed under the MIT license.
@@ -32,42 +32,42 @@ Raphael.fn.tachometer = function(value, opt) {
     this.drawNeedle(canvas, cx, cy, sb, value, opt);
   };
   
-  T.drawBoard = function(c, x, y, s, r, f) {
-    c.circle(x, y, r).attr(opt.outerFrameAttr);
-    c.circle(x, y, r - f).attr(opt.innerFrameAttr);
-    c.circle(x, y, r - f * 2).attr(opt.boardAttr);
+  T.drawBoard = function(c, cx, cy, s, r, f) {
+    c.circle(cx, cy, r).attr(opt.outerFrameAttr);
+    c.circle(cx, cy, r - f).attr(opt.innerFrameAttr);
+    c.circle(cx, cy, r - f * 2).attr(opt.boardAttr);
   };  
 
-  T.drawScale = function(c, x, y, sb, opt) {
+  T.drawScale = function(c, cx, cy, sb, opt) {
     var w = opt.scaleWidth,
         h = opt.scaleLength,
         m = opt.boardMargin,
         sw = Math.ceil(w / 2),
         sh = Math.ceil(h / 2);
     
-    var ss = this.scale(c, x, y, sw, sh, sb, m, opt.scaleAngleStart);
-    this.cloneScale(ss, x, y, opt.shortScaleCount, opt);
+    var ss = this.scale(c, cx, cy, sw, sh, sb, m, opt.scaleAngleStart);
+    this.cloneScale(ss, cx, cy, opt.shortScaleCount, opt);
     
-    var ls = this.scale(c, x, y, w, h, sb, m, opt.scaleAngleStart);
-    this.cloneScale(ls, x, y, opt.longScaleCount, opt);
+    var ls = this.scale(c, cx, cy, w, h, sb, m, opt.scaleAngleStart);
+    this.cloneScale(ls, cx, cy, opt.longScaleCount, opt);
   };
   
-  T.scale = function(c, x, y, w, h, sb, m, r) {
-    var sx = x - Math.floor(w / 2),
+  T.scale = function(c, cx, cy, w, h, sb, m, r) {
+    var sx = cx - Math.floor(w / 2),
         sy = sb - h - m;
     
-    return c.rect(sx, sy, w, h).attr(opt.scaleAttr).rotate(r, x, y);
+    return c.rect(sx, sy, w, h).attr(opt.scaleAttr).rotate(r, cx, cy);
   };
   
-  T.cloneScale = function(s, x, y, count, opt) {
+  T.cloneScale = function(s, cx, cy, count, opt) {
     var rad = (opt.scaleAngleEnd - opt.scaleAngleStart) / count;
     
     for (var i=1 ; i <= count ; i++) {
-      s.clone().rotate(rad * i, x, y);
+      s.clone().rotate(rad * i, cx, cy);
     }
   };
   
-  T.drawNumber = function(c, x, y, sb, opt) {
+  T.drawNumber = function(c, cx, cy, sb, opt) {
     var lc = opt.longScaleCount,
         rad = (opt.scaleAngleEnd - opt.scaleAngleStart) / lc,
         prec = (opt.numberMax - opt.numberMin) / lc,
@@ -75,31 +75,31 @@ Raphael.fn.tachometer = function(value, opt) {
         pos = sb / 2 - m,
         ss = opt.scaleAngleStart,
         label = null,
-        cx = null,
-        cy = null;
+        x = null,
+        y = null;
     
     for (var i=0 ; i <= lc ; i++) {
       label = Math.round(prec * i) + opt.numberMin;
       
       if (i == 0) label = label + opt.numberUnit;
       
-      cx = x + Math.floor(pos * Math.cos(Raphael.rad(rad * i + 90 + ss)));
-      cy = y + Math.floor(pos * Math.sin(Raphael.rad(rad * i + 90 + ss)));
+      x = cx + Math.floor(pos * Math.cos(Raphael.rad(rad * i + 90 + ss)));
+      y = cy + Math.floor(pos * Math.sin(Raphael.rad(rad * i + 90 + ss)));
       
-      c.text(cx, cy, label).attr(opt.numberAttr).toFront();
+      c.text(x, y, label).attr(opt.numberAttr).toFront();
     }
   };
   
-  T.drawNeedle = function(c, x, y, sb, val, opt) {
+  T.drawNeedle = function(c, cx, cy, sb, val, opt) {
     var xoffset = Math.floor(opt.needlePivotWidth / 2),
         m = opt.needleMargin,
         xpos = {
-          start: x + xoffset,
-          middle: x,
-          end: x - xoffset
+          start: cx + xoffset,
+          middle: cx,
+          end: cx - xoffset
         },
         ypos = {
-          start: y,
+          start: cy,
           end: sb - m
         },
         path = 'M' + xpos.start + ' ' + ypos.start + ' ' +
@@ -107,26 +107,54 @@ Raphael.fn.tachometer = function(value, opt) {
                'L' + xpos.end + ' ' + ypos.start + ' ' +
                'L' + xpos.start + ' ' + ypos.start,
         needle = c.path(path).attr(opt.needleAttr),
-        outer = c.circle(x, y, m).attr(opt.outerNeedlePivotAttr),
-        inner = c.circle(x, y, m / 2).attr(opt.innerNeedlePivotAttr),
-        set = c.set(),
+        outer = c.circle(cx, cy, m).attr(opt.outerNeedlePivotAttr),
+        inner = c.circle(cx, cy, m / 2).attr(opt.innerNeedlePivotAttr),
+        hand = c.set(),
         ss = opt.scaleAngleStart,
         se = opt.scaleAngleEnd,
         ns = opt.numberMin,
-        ne = opt.numberMax,
-        perc = (se - ss) * ((val - ns) / (ne - ns)) + ss;
+        ne = opt.numberMax;
     
-    needle.rotate(ss, x, y);
-    set.push(needle, outer, inner).attr(opt.needleSetAttr);
+    hand.push(needle, outer, inner).attr(opt.handAttr).rotate(ss, cx, cy);
     
-    if (opt.needleAnimation) {
-      set.animate({
-        transform: "r" + perc + " " + x + " " + y
-      }, 700, 'bounce');
+    this.p = { 
+      value: ns,
+      hand: hand,
+      animation: opt.needleAnimation,
+      duration: opt.needleAnimationDuration,
+      easing: opt.needleAnimationEasing,
+      cx: cx,
+      cy: cy,
+      ss: ss,
+      se: se,
+      ns: ns,
+      ne: ne
+    };
+    
+    this.set(val);
+  };
+  
+  T.set = function(val, anim) {
+    var p = this.p;
+    
+    // fix value in number between numberMin and numberMax;
+    val = (p.ne < val) ? p.ne : ((val < p.ns) ? p.ns : val);
+    
+    var perc = (p.se - p.ss) * ((val - p.ns) / (p.ne - p.ns)) + p.ss,
+        tstr = "r" + perc + " " + p.cx + " " + p.cy;
+    
+    this.p.value = val;
+    
+    if (typeof anim != 'undefined' ? anim : p.animation) {
+      p.hand.animate({transform: tstr}, p.duration, p.easing);
     }
     else {
-      set.rotate(perc, x, y);
-    }
+      p.hand.transform(tstr);
+    }    
+  };
+  
+  T.get = function() {
+    return this.p.value;
   };
   
   function merge(dest, src) {
@@ -170,7 +198,7 @@ Raphael.fn.tachometer = function(value, opt) {
       'fill': '#fff',
       'stroke-width': 0
     },
-    needleSetAttr: {
+    handAttr: {
       'stroke-width': 0.3,
       'stroke': '#fff'
     },
